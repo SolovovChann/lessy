584648ee2cdf3417e6a0ec5f393f0730f9e6a0af
SolovovChann <solnik2001@gmail.com>
2021-09-27 22:00:36 +0500

    Add logo

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 
 import '../less/style.less'
 import '../templates/index.pug'
+import '../assets/img/logo.png'
 
 document.querySelectorAll('.form-checkbox').forEach(checkbox => {
   let input = checkbox.querySelector('input')
@@ -11,11 +12,13 @@ document.querySelectorAll('.form-checkbox').forEach(checkbox => {
   input.addEventListener('change', () => checkbox.classList.toggle('active'))
 })
 
-document.querySelectorAll('.form-radio').forEach(radio => {
+document.querySelectorAll('.form-radio:not(.disabled)').forEach(radio => {
   let input = radio.querySelector('input')
 
   input.addEventListener('change', () => {
-    document.querySelectorAll('.form-radio').forEach(sibbling => sibbling.classList.remove('active'))
+    document
+      .querySelectorAll('.form-radio')
+      .forEach(sibbling => sibbling.classList.remove('active'))
     radio.classList.add('active')
   })
 })
